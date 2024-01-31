7660497eb3035330cabed9c5f126e0b978c7209f
Sim P <anomis.p@gmail.com>
2024-01-31 20:18:49 +0100

    ukol c.1 index.html a script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.body.innerHTML = "<h1>Lekce 2</h1>"
+
+document.body.innerHTML = "<h2>Hazeni kostkou</h2>"
+
+let cislo = Math.floor(Math.random() * 6) + 1
+document.body.innerHTML = cislo
